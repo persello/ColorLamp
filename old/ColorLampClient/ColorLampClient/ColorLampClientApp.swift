287dc1b411afc0e149f8b2cf9f98400b131f3064
Riccardo Persello <riccardo.persello@icloud.com>
2023-09-03 15:02:53 +0200

    Added old client, was a submodule somehow.

---
+++ old/ColorLampClient/ColorLampClient/ColorLampClientApp.swift
@@ -0,0 +1,20 @@
+//
+//  ColorLampClientApp.swift
+//  ColorLampClient
+//
+//  Created by Riccardo Persello on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorLampClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+                    .navigationTitle("Color Lamp")
+            }
+        }
+    }
+}
